dcd940307253865a34335a019bd92af9772b515d
Pham Tuan <phamtuan199911@gmail.com>
2021-04-09 14:21:46 +0700

    Add central init function

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package common
+
+import (
+	_ "github.com/Nubes3/common/config"
+	"github.com/Nubes3/common/models/arangodb"
+	"github.com/Nubes3/common/models/nats"
+	"github.com/Nubes3/common/models/seaweedfs"
+	"github.com/Nubes3/common/models/stan"
+)
+
+func InitCoreComponents(initArango, initSeaweedfs, initStan, initNats bool) {
+	if initArango {
+		arangodb.InitArango()
+	}
+	if initSeaweedfs {
+		clean, err := seaweedfs.InitFs()
+		if err != nil {
+			panic(err)
+		}
+		defer clean()
+	}
+	if initNats {
+		clean, err := nats.InitNats()
+		if err != nil {
+			panic(err)
+		}
+		defer clean()
+	}
+	if initStan {
+		clean, err := stan.InitStan()
+		if err != nil {
+			panic(err)
+		}
+		defer clean()
+	}
+}
