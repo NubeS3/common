5f3a5867700892723552c9e1868163cb91928405
Pham Tuan <phamtuan199911@gmail.com>
2021-04-11 09:59:37 +0700

    Add response for message

--- models/nats/msg_structure.go
+++ models/nats/msg_structure.go
@@ -2,6 +2,8 @@
 
 package nats
 
+import "github.com/Nubes3/common/utils"
+
 const (
 	GetById = iota
 	Resolve
@@ -13,4 +15,15 @@ type Msg struct {
 	ReqType   ReqType `json:"req_type"`
 	Data      string  `json:"data"`
 	ExtraData *string `json:"extra_data,omitempty"`
+}
+
+type MsgResponse struct {
+	IsErr     bool   `json:"is_err"`
+	Data      string `json:"data"`
+	ExtraData string `json:"extra_data"`
+}
+
+type ErrMsg struct {
+	ErrType utils.ErrorType `json:"err_type"`
+	Message string          `json:"message"`
 }
